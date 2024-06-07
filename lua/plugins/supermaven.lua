4222e98a701b858b3aef2a5bf5c79acdfd139b26
hasoy <hamid.soy@hotmail.com>
2024-06-07 23:31:54 +0200

    adjust supermaven for ctrl and j

---
+++ lua/plugins/supermaven.lua
@@ -0,0 +1,21 @@
+return {
+  {
+    "supermaven-inc/supermaven-nvim",
+    config = function()
+      require("supermaven-nvim").setup({
+        keymaps = {
+          accept_suggestion = "<C-j>",
+          clear_suggestion = "<C-]>",
+          accept_word = "<Tab-Space>",
+        },
+        ignore_filetypes = { cpp = true },
+        color = {
+          suggestion_color = "#7B1FA2",
+          cterm = 244,
+        },
+        disable_inline_completion = false, -- disables inline completion for use with cmp
+        disable_keymaps = false, -- disables built in keymaps for more manual control
+      })
+    end,
+  },
+}
