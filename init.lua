172a489e879255c5567c6f8d9725c0234a266e7e
hasoy <ham@nlleq0413001509.corp.capgemini.com>
2024-08-15 11:14:02 +0200

    changes

--- init.lua
+++ init.lua
@@ -1,3 +1,15 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
-vim.api.nvim_set_option("clipboard", "unnamedplus")
+--
+vim.g.clipboard = {
+  name = "WslClipboard",
+  copy = {
+    ["+"] = "clip.exe",
+    ["*"] = "clip.exe",
+  },
+  paste = {
+    ["+"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+    ["*"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+  },
+  cache_enabled = 0,
+}
--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -35,9 +35,8 @@ return {
           ["<CR>"] = cmp.config.disable,
         }),
         sources = cmp.config.sources({
+          { name = "luasnip" },
           { name = "nvim_lsp" },
-          { name = "luasnip" }, -- For luasnip users.
-          { name = "supermaven" },
         }, {
           { name = "buffer" },
         }),
