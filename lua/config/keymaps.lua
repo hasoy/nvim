1b548404f4ea044d92fdf1352110cb016376c50d
Hamid Soy <abdul-hamid.soy@capgemini.com>
2025-01-14 13:48:49 +0100

    remap ctrl l

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -36,7 +36,7 @@ vim.keymap.set(
   { noremap = true, silent = true, desc = "Previous diagnostic" }
 )
 
-vim.keymap.set("n", "<C-l>", "<cmd>e#<cr>", { noremap = true, silent = true, desc = "Previous open buffer" })
+vim.keymap.set("n", "<C-:>", "<cmd>e#<cr>", { noremap = true, silent = true, desc = "Previous open buffer" })
 
 -- Move Lines
 local moveline = require("moveline")
