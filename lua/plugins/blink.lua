3b3716dea20a31beccff1db3f10346bed83576ba
ham <hamid.soy@hotmail.com>
2025-02-18 22:16:50 +0100

    adjust plugins

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -18,9 +18,9 @@ return {
     -- See the full "keymap" documentation for information on defining your own keymap.
     keymap = {
       preset = "default",
+      ["<Tab>"] = { "select_and_accept" },
       ["Up"] = { "select_prev", "fallback" },
       ["Down"] = { "select_next", "fallback" },
-      ["Tab"] = { "select_and_accept" },
     },
 
     appearance = {
@@ -36,7 +36,7 @@ return {
     -- Default list of enabled providers defined so that you can extend it
     -- elsewhere in your config, without redefining it, due to `opts_extend`
     sources = {
-      default = { "lsp", "path", "snippets", "buffer" },
+      default = { "lsp", "snippets", "path", "buffer" },
     },
   },
   opts_extend = { "sources.default" },
