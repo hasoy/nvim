854f4320aa869970960b0a24201b7f68a77a3226
hasoy <hamid.soy@hotmail.com>
2026-03-23 11:12:24 +0100

    fix moveline and spider

--- lua/plugins/moveline.lua
+++ lua/plugins/moveline.lua
@@ -1,4 +1,10 @@
 return {
-  "willothy/moveline.nvim",
-  build = "make",
+  "fedepujol/move.nvim",
+  opts = {},
+  keys = {
+    { "<A-Up>", "<cmd>MoveLine(-1)<CR>", mode = "n" },
+    { "<A-Down>", "<cmd>MoveLine(1)<CR>", mode = "n" },
+    { "<A-Up>", "<cmd>MoveBlock(-1)<CR>", mode = "v" },
+    { "<A-Down>", "<cmd>MoveBlock(1)<CR>", mode = "v" },
+  },
 }
--- lua/plugins/spider.lua
+++ lua/plugins/spider.lua
@@ -2,5 +2,17 @@ return {
   {
     "chrisgrieser/nvim-spider",
     lazy = true,
+    opts = {
+      skipInsignificantPunctuation = true,
+      subwordMovement = true,
+      consistentOperatorPending = false,
+      customPatterns = {},
+    },
+    keys = {
+      { "w", "<cmd>lua require('spider').motion('w')<CR>", mode = { "n", "o", "x" } },
+      { "e", "<cmd>lua require('spider').motion('e')<CR>", mode = { "n", "o", "x" } },
+      { "b", "<cmd>lua require('spider').motion('b')<CR>", mode = { "n", "o", "x" } },
+      { "ge", "<cmd>lua require('spider').motion('ge')<CR>", mode = { "n", "o", "x" } },
+    },
   },
 }
