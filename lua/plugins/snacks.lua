8a2a2c04d5477d8f97ce85f6bc507d51e972a16a
Hamid Soy <hamid.soy@hotmail.com>
2026-05-10 14:27:49 +0200

    only add multiple items to the QF list and always open the first itme

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -2,17 +2,26 @@ return {
   "folke/snacks.nvim",
   opts = {
     picker = {
+      actions = {
+        smart_enter = function(picker)
+          if #picker:selected() > 0 then
+            picker:action("qflist")
+          else
+            picker:action("confirm")
+          end
+        end,
+      },
       win = {
         input = {
           keys = {
             ["<M-q>"] = { "qflist", mode = { "i", "n" } },
-            ["<CR>"] = { "qflist", mode = { "i", "n" } },
+            ["<CR>"] = { "smart_enter", mode = { "i", "n" } },
           },
         },
         list = {
           keys = {
             ["<M-q>"] = { "qflist", mode = { "i", "n" } },
-            ["<CR>"] = { "qflist", mode = { "n" } },
+            ["<CR>"] = { "smart_enter", mode = { "n" } },
           },
         },
       },
