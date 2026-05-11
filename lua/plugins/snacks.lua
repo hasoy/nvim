9e3734b35035eb642f5551a83d770d3a5fdf998c
hasoy <hamid.soy@hotmail.com>
2026-05-11 13:41:05 +0200

    search and grep in cwd instead of root folder

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -1,5 +1,13 @@
 return {
   "folke/snacks.nvim",
+  keys = {
+    { "<leader><space>", function() Snacks.picker.smart({ root = false }) end, desc = "Find Files (cwd)" },
+    { "<leader>ff", function() Snacks.picker.files({ root = false }) end, desc = "Find Files (cwd)" },
+    { "<leader>fF", function() Snacks.picker.files() end, desc = "Find Files (Root Dir)" },
+    { "<leader>/", function() Snacks.picker.grep({ root = false }) end, desc = "Grep (cwd)" },
+    { "<leader>sg", function() Snacks.picker.grep({ root = false }) end, desc = "Grep (cwd)" },
+    { "<leader>sG", function() Snacks.picker.grep() end, desc = "Grep (Root Dir)" },
+  },
   opts = {
     picker = {
       actions = {
